59978ba0ba59b1f80228e52161b5bae7581c69ba
Ghadeer-Alkhataybeh <eng.ghadeer.alkhataybeh@gmail.com>
2018-11-01 16:44:21 +0200

    first and last edtion

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,94 @@
 // YOUR CODE HERE:
+let app = {}
+$(document).ready(() => {
+	app.server = 'http://parse.rbk.hackreactor.com/chatterbox/classes/messages';
+	app.init = () => {};
+	app.send = (message) => {    // this message represant object 
+		$.ajax({
+			url :app.server,
+			type: 'POST',
+			data: JSON.stringify(message),
+			contentType: 'application/json',
+			success : (data) => {
+				console.log('success', data);
+				$('#chats').html('');
+				app.fetch();
+			},
+			error: (data) => {
+				console.log("Failed to send Message ", data);
+			} 
+		})
+	};
+	app.fetch = () => {
+		// $.get(app.server, (data) => {
+		// 	console.log(data);
+
+		// });
+		$.ajax({
+			url :app.server,
+			type: 'GET',
+			data: {order: '-createdAt'},
+			contentType: 'application/json',
+			success : (data) => {
+				// iterative to render all the message
+				data.results.forEach((elem) => {
+					app.renderMessage(elem);
+					app.addRooms(elem);
+					 // we are only passing the rome name from each element which is the passed message
+					});
+				// add the room
+				console.log('successfuly retrived', data);
+			},
+			error: (data) => {
+				console.log("Failed during fetch messages ", data);
+			} 
+		})
+
+	};
+	app.clearMessages = () => {
+		$('#chats').text(''); //  clear(delete) all the message inside the div of chats
+	};
+	app.renderMessage = (message) => {
+		let $containerMsg = $(`<div class ="chat" id=${message.username}></div>`); // we use message.username as uniqe id for eacg message;
+		let $label = $(`<label class="username" id="user">@${message.username}</label>`);  // using ES6
+		 //  var $label = $('<label class="username" id="user">'+message.username+'</label>');
+		 // $('#user').text(message.username);
+         //$('#message').text(message.text); 
+         let $text = $(`<p id="message">${message.text}</p>`); 
+         $containerMsg.append($label);
+         $containerMsg.append($text);
+         $('#chats').append($containerMsg);
+     };
+	//{roomname1: [message1,meeessage], ghjh:[],
+	// roomname2 : .....   }
+	app.rooms = {};
+	app.addRooms = (message) => {
+		if (!app.rooms[message.roomname]) {
+			app.rooms[message.roomname] = [message];
+      	app.renderRoom(message.roomname) //
+
+      }
+      else {
+      	app.rooms[message.roomname].push(message)
+      }
+  }
+  app.renderRoom = (room) => { 
+				let $selectRoom = $(`<option value = "${room}" >${room}</option>`) // don't forget the back tic " ` " Ghadeer
+				$("#roomSelect").append($selectRoom);	
+			}
+// call fetch function to retrive the rata from erver
+app.fetch();
+// button to send message
+$('#submit-btn').on('click', () => {
+	let message ={};
+	message.text = $('#message-input').val(); 
+	let getUserName = window.location.search.substring(window.location.search.indexOf('username'));
+	message.username = getUserName.substring(getUserName.indexOf('=')+1);
+	message.roomname = "RBK 5";
+	app.send(message);
+})
+
+
+ // we have to call it so it can run auto when our app run 
+
+});
